235a4a92046465057cada5d1ae27925d03318dc9
Kusuri Dheeraj Kumar <kusuri.dheeraj2014@gmail.com>
2021-09-02 22:14:48 +0530

    Added Meeting Screens

--- lib/screens/meetingscreen.dart
+++ lib/screens/meetingscreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:video_time/variables.dart';
+import 'superman';
 
 class MeetingScreen extends StatefulWidget {
   // const MeetingScreen({ Key? key }) : super(key: key);
@@ -8,8 +10,58 @@ class MeetingScreen extends StatefulWidget {
 }
 
 class _MeetingScreenState extends State<MeetingScreen> {
+    with SingleTickerProviderStateMixin {
+  TabController tabController;
+  tabBuilder(String name) {
+    return Container(
+      width: 150,
+      height: 50,
+      child: Card(
+        child: Center(
+          child: Text(
+            name,
+            style: ralewayStyle(
+              15,
+              Colors.black,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Zoom Clone",
+          style: ralewayStyle(20, Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlue,
+        bottom: TabBar(
+          controller: tabController,
+          tabs: [
+            tabBuilder("Join Meeting"),
+            tabBuilder("Create Meeting"),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: tabController,
+        children: [
+          JoinMeetingScreen(),
+          CreateMeetingScreen(),
+        ],
+      ),
+    );
   }
+}
 }
